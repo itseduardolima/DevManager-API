65fcb76dc67aa0569d229d70f1b762e1ffc4acc1
eduardo.castro <eduardo.castro@conecthus.org.br>
2023-08-17 13:10:57 -0400

    fix

--- src/projects/shared/projects.service.ts
+++ src/projects/shared/projects.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import { CreateProjectDto } from './dto/create-project.dto';
-import { UpdateProjectDto } from './dto/update-project.dto';
+import { CreateProjectDto } from '../dto/create-project.dto';
+import { UpdateProjectDto } from '../dto/update-project.dto';
+
 
 @Injectable()
 export class ProjectsService {
